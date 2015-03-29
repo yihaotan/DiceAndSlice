f882233d2179be02b67e878498b650d08f4de3f0
yihaotan2012 <yihaotan2012@sis.smu.edu.sg>
2015-03-29 17:23:26 +0800

    Magic

--- web/main.js
+++ web/main.js
@@ -2,42 +2,64 @@ function generateCharts(){
     
     airlineEmissionChart = dc.barChart('#dc-airline-emission');
     airlineBubbleChart = dc.bubbleChart('#dc-airline-bubble');
-    //airlineProfitChart = dc.lineChart('#dc-airline-profitLine');
-    //airlineTaxChart = dc.lineChart('#dc-airline-taxLine');
-    //airlineAfterTaxChart = dc.lineChart('#dc-airline-afterTaxLine');
-    airlineCompositeChart = dc.compositeChart('#dc-airline-compositeLine')
-     
-     
-    //count inconsistent (.0) for the count not needed
+    airlineDistanceChart = dc.barChart('#dc-airline-distance');
+    airlineCompositeChart = dc.compositeChart('#dc-airline-compositeLine');
+    //airlineProfitChart = dc.lineChart("#dc-airline-profitChart");
+    //airlineTaxChart = dc.lineChart("#dc-airline-taxChart");
+    var tier1Limit = 5717 * 1000;
+    var tier2Limit = 27301 * 1000;
+    var tier3Limit = 60983 * 1000;
+    var tier1Value =($("#tier1 :selected").val());
+    var tier2Value = ($("#tier2 :selected").val());
+    var tier3Value = ($("#tier3 :selected").val());
+    var tier4Value = ($("#tier4 :selected").val());
+    
+    //count inconsistent (0) for the count not needed
     //color hardcode for bubblechart in dc.js
     //put legend for dc.js 
      d3.csv('final.csv',function(data){
         //read the csv file
         var emissionArray = [];
-        //var distanceArray = [];
-        //var taxArray =[];
-        //var profitArray =[];
-        var airlineDistanceMap ={};
+        var distanceArray = [];
+        var profitArray = [];
+        var taxArray = [];
+        //var airlineKeyArray = [];
+        var airlineDistanceMap = {};
         var airlineEmissionMap = {};
         var airlineProfitMap = {};
+        
         data.forEach(function(d){
             d.airlineName = d.Airline_Unique_Name;
             d.airlineKey = d.Airline_Iata_Faa;
-            d.profit = Math.round(+(d.Total_Op_Profit_Loss_2013));
+            d.profit = Math.round(+(d.Total_Op_Profit_Loss_2013)) * 1000;
             d.distance = Math.round(+(d.Total_Distance_In_Km));
-            d.emission = Math.round(+(d.Total_Emission));
-            d.tax = 5 * d.emission;
+            d.emission = Math.round(+(d.Total_Emission)) * 1000;
+            d.tax = 0;
+            if (d.emission < tier1Limit){
+                d.tax = tier1Value * d.emission;
+            }else if(d.emission < tier2Limit){
+                d.tax = tier2Value * d.emission;
+            }else if(d.emission < tier3Limit){
+                d.tax = tier3Value * d.emission;
+            }else{
+                d.tax = tier4Value * d.emission;
+            }
             d.afterTax = d.profit - d.tax;
             d.ratio = +(d.emission / d.distance);
+            taxArray.push(d.tax);
             emissionArray.push(d.emission);
+            distanceArray.push(d.distance);
+            profitArray.push(d.profit);
+            //For the count 
             airlineDistanceMap[d.airlineKey] = d.distance;
             airlineEmissionMap[d.airlineKey] = d.emission;
             airlineProfitMap[d.airlineKey] = d.profit;
-            
+           
         });
+      
         var facts = crossfilter(data);
         var all = facts.groupAll();
-       
+        
         function getMaxProfit() {
             var maxProfit = d3.max(data, function (d) {
                 return d.profit;
@@ -153,17 +175,42 @@ function generateCharts(){
             }    
         );
         emissionArray = crossfilter.quicksort(emissionArray,0,emissionArray.length);
+        distanceArray = crossfilter.quicksort(distanceArray,0,distanceArray.length);
+        profitArray = crossfilter.quicksort(profitArray,0,profitArray.length);
         
-        var sortByEmission = crossfilter.quicksort.by(function(d){return -d.emission;});
-        var sortByEmissionData = sortByEmission(data,0,data.length);
+        //sort the distance domain in descending order
+        var airlineDistanceDomain = [];
+        var sortable = [];
+        for (var airlineKey in airlineDistanceMap)
+            sortable.push([airlineKey, airlineDistanceMap[airlineKey]]);
+            sortable.sort(function(a, b) {return b[1] - a[1]});
+        for (var i = 0 ; i < 43 ; i++){
+          
+           airlineDistanceDomain.push(sortable[i][0]);
+        }
         
-     
-        var airlineDistanceDimension = facts.dimension(function(d){return d.airlinKey;});
+        //sort the emission domain in descending order
+        var airlineEmissionDomain = [];
+        var sortable1 = [];
+        for (var airlineKey in airlineEmissionMap)
+            sortable1.push([airlineKey, airlineEmissionMap[airlineKey]]);
+            sortable1.sort(function(a, b) {return b[1] - a[1]});
+        for (var i = 0 ; i < 43 ; i++){
+          
+           airlineEmissionDomain.push(sortable1[i][0]);
+        }
+        //sort the emission in descending order previous method
+        //var sortByEmission = crossfilter.quicksort.by(function(d){return -d.emission;});
+        //var sortByEmissionData = sortByEmission(data,0,data.length);
+        
+        var fakeDimension = facts.dimension(function(d){return d.airlinKey;});
+        
+        var airlineDistanceDimension = facts.dimension(function(d){return d.airlineKey;});
         var airlineDistanceGroup = airlineDistanceDimension.group().reduceSum(function(d){return d.distance;});
         
         var airlineEmissionDimension = facts.dimension(function(d){return d.airlineKey;});
         var airlineEmissionGroup = airlineEmissionDimension.group().reduceSum(function(d){return d.emission;});
-        
+       
         var airlineProfitDimension = facts.dimension(function(d){return d.airlineKey;});
         var airlineProfitGroup = airlineProfitDimension.group().reduceSum(function(d){return d.profit;});
         
@@ -214,11 +261,10 @@ function generateCharts(){
                 return profitSum;
             });
         }
-        //plot the airline emission column chart
-        //need to rotate the axis label
+        
         airlineEmissionChart.width(990)
                 .height(300)
-                .margins({top: 10, right: 10, bottom: 40, left: 50})
+                .margins({top: 10, right: 10, bottom: 40, left: 70})
                 .dimension(airlineEmissionDimension)
                 .group(airlineEmissionGroup)
                 .transitionDuration(10)
@@ -242,24 +288,74 @@ function generateCharts(){
                 })
                  .barPadding(0.05)
                 .outerPadding([1])
-                .x(d3.scale.ordinal().domain(data.map(function (d) {
-                    return d.airlineKey;
-                })))
+                .x(d3.scale.ordinal().domain(airlineEmissionDomain))
                 .elasticY(true)
                 .xUnits(dc.units.ordinal)
                 .xAxis().tickFormat();
         //airlineEmissionChart.onClick = function() {};
+        //plot airline distance chart
+        airlineDistanceChart.width(990)
+            .height(300)
+            .margins({top: 10, right: 0, bottom: 40, left:70})
+            .dimension(airlineDistanceDimension)
+            .group(airlineDistanceGroup)
+            .transitionDuration(10)
+            .xAxisLabel('Airlines') 
+            .yAxisLabel('Distance / Km')
+            .renderHorizontalGridLines(true)
+            .renderTitle(true)
+            .title(function (d) {
+                return  d.key + ": " + d.value;
+            })
+            .barPadding(0.05)
+            .outerPadding([1])
+             .x(d3.scale.ordinal().domain(airlineDistanceDomain))
+            .xUnits(dc.units.ordinal)
+            .colors(d3.scale.ordinal().domain(["0-25", "26-50", "51-75", "75-100"]).range(['rgb(255,255,212)','rgb(254,217,142)','rgb(254,153,41)','rgb(204,76,2)']))
+            .colorAccessor(function (d) {
+                    if (d.value < distanceArray[10]) {
+                        return "0-25";
+                    } else if (d.value < distanceArray[21]) {
+                        return "26-50";
+                    } else if (d.value < distanceArray[32]) {
+                        return "51-75";
+                    }
+                    return "75-100";
+            })
+            .elasticY(true)
+            .xAxis().tickFormat();
+            
         //plot the bubble chart x,y domain not fixed yet
          airlineBubbleChart
             .width(990) 
             .height(500)  
-            .margins({top: 10, right: 10, bottom: 40, left: 50})
+            .margins({top: 10, right: 10, bottom: 40, left: 70})
             .dimension(bubbleDimension)
             .group(bubbleGroup)
-            .colors(colorbrewer.RdYlGn[9]) 
-            .colorDomain([getMinProfit(), getMaxProfit()]) 
+            .colors(d3.scale.ordinal().domain(["0","1","2","3","4","5","6","7","8"]).range(colorbrewer.RdYlGn[9])) 
+            //.colorDomain([getMinProfit(), getMaxProfit()]) 
             .colorAccessor(function (d) {
-                return d.value.profit;
+                if (d.value.profit < profitArray[5]){
+                    return "0";
+                }else if(d.value.profit < profitArray[10]){
+                    return "1";
+                }else if(d.value.profit < profitArray[15]){
+                    return "2";
+                }else if(d.value.profit < profitArray[20]){
+                    return "3";
+                }else if(d.value.profit < profitArray[25]){
+                    return "4";
+                }else if(d.value.profit < profitArray[30]){
+                    return "5";
+                }else if(d.value.profit < profitArray[35]){
+                    return "6";
+                }else if(d.value.profit < profitArray[40]){
+                    return "7";
+                }else{
+                    return "8";
+                }
+                
+                  
             })
             .keyAccessor(function (d) {
                 return d.value.distance;
@@ -270,11 +366,11 @@ function generateCharts(){
             .radiusValueAccessor(function (d) {
                 return d.value.ratio;
             })
-            .maxBubbleRelativeSize(20)
+            .maxBubbleRelativeSize(1)
             .x(d3.scale.linear().domain([getMinDistance(), getMaxDistance()]))
             .y(d3.scale.linear().domain([getMinEmission(), getMaxEmission()]))
-            .r(d3.scale.linear().domain([0,20]))
-            .yAxisPadding(200000)
+            .r(d3.scale.linear().domain([0,1000]))
+            .yAxisPadding(200000000)
             .xAxisPadding(5000000)
             .elasticX(true)
             .elasticY(true)
@@ -293,30 +389,25 @@ function generateCharts(){
             
             
         airlineProfitChart = dc.lineChart(airlineCompositeChart)
-            .group(airlineProfitGroup,"BeforeTax Profit")
+            .group(airlineProfitGroup,"Pre-Tax Profit")
             .renderDataPoints({radius:3})
-            .colors("#2ca25f")
-            //.renderArea(true)
-    
-        airlineTaxChart = dc.lineChart(airlineCompositeChart)
-           .group(airlineTaxGroup,"Tax")
-            .renderDataPoints({radius:3})
-            .colors("#3182bd")
-            //.renderArea(true)
-          
-        airlineAfterTaxChart = dc.lineChart(airlineCompositeChart)
-            .group(airlineAfterTaxGroup,"AfterTax Profit")
-            .renderDataPoints({radius:3})
-            .colors("#e6550d")
-            
-            //.renderArea(true)
-            
+            .colors("#3182bd");
            
+        airlineTaxChart = dc.lineChart(airlineCompositeChart)
+             .group(airlineTaxGroup,"Tax")
+            .renderDataPoints({radius:3})
+            .colors("#e6550d");
+            
+        airlineAfterTaxChart = dc.lineChart(airlineCompositeChart)
+            .group(airlineAfterTaxGroup,"After-Tax Profit")
+            .renderDataPoints({radius:3})
+            .colors("#2ca25f");
+            
         airlineCompositeChart
                 .width(990)
                 .height(300)
-                .margins({top: 10, right: 10, bottom: 40, left: 60})
-                .dimension(airlineDistanceDimension)
+                .margins({top: 10, right: 10, bottom: 40, left: 80})
+                .dimension(fakeDimension)
                 .x(d3.scale.ordinal().domain(data.map(function (d) {return d.airlineKey;}).sort()))
                 .xUnits(dc.units.ordinal)
                 .yAxisLabel("USD")
@@ -329,89 +420,10 @@ function generateCharts(){
                       .attr('dx', '-10');
                 
                 })
-                        
                 .elasticY(true)
                 .compose([airlineProfitChart,airlineTaxChart,airlineAfterTaxChart])
-                .legend(dc.legend().x(880).y(10).itemHeight(13).gap(5))
-        
-        //plot the composite graph
-        /*airlineCompositeChart
-            .width(990)
-            .height(300)
-            .margins({top: 10, right: 10, bottom: 40, left: 60})
-            .dimension(airlineDistanceDimension)
-            .x(d3.scale.ordinal().domain(data.map(function (d) {return d.airlineKey;}).sort()))
-            .xUnits(dc.units.ordinal)
-            .yAxisLabel("USD")
-            //.y(d3.scale.linear().domain([getMinAfterTax()-500000, getMaxAfterTax()+500000]))
-             .elasticY(true)
-            .xAxisLabel("Airlines")
-            .legend(dc.legend().x(80).y(20).itemHeight(13).gap(5))
-             //.shareTitle(false)
-            .yAxisPadding(200000)
-            .renderTitle(true)
-            .renderHorizontalGridLines(true)
-            .compose([
-                dc.lineChart(airlineCompositeChart)
-                    .dimension(airlineProfitDimension)
-                    .group(airlineProfitGroup,'Profit')
-                    .x(d3.scale.ordinal().domain(data.map(function (d) {return d.airlineKey;}).sort()))
-                    .xUnits(dc.units.ordinal)
-                    .colors('red')
-                    .title(function(d){
-                        return d.key +":" +d.value;
-                    })
-                    .renderTitle(true)
-                    .renderDataPoints({radius:5})
-                        
-                ,dc.lineChart(airlineCompositeChart)
-                    .dimension(airlineTaxDimension)
-                    .group(airlineTaxGroup,"Tax")
-                    .x(d3.scale.ordinal().domain(data.map(function (d) {return d.airlineKey;}).sort()))
-                    .xUnits(dc.units.ordinal)
-                    .title(function(d){
-                        return d.key +":" +d.value;
-                    })
-                    .renderTitle(true)
-                    .renderDataPoints({radius:5})
-                    .colors('blue')
-                    
-                    
-
-                ])*/
-                
-
+                .legend(dc.legend().x(220).y(280).itemWidth(95).legendWidth(600).horizontal(true))
        
-        /*airlineCompositeChart.width(900)
-            .height(300)
-            .margins({top: 10, right: 10, bottom: 30, left:55})
-            .dimension(airlineAfterTaxDimension)
-            .y(d3.scale.linear().domain([getMinAfterTax()-500000, getMaxAfterTax()+500000]))
-            .x(d3.scale.ordinal().domain(data.map(function (d) {return d.airlineKey;}).sort()))
-            .xUnits(dc.units.ordinal)
-            .shareTitle(true)
-            .compose([dc.lineChart(airlineCompositeChart)
-                     .interpolate('linear')
-                     .group(airlineProfitGroup,'Before Tax')
-                      .colors('green')
-                      .renderTitle(true)
-                     
-             ,
-             dc.lineChart(airlineCompositeChart)
-                     .interpolate('linear')
-                     .group(airlineTaxGroup,'Tax')
-                     .colors('blue')
-                     .renderTitle(true) 
-             ,
-             dc.lineChart(airlineCompositeChart)
-                     .interpolate('linear')
-                     .group(airlineAfterTaxGroup,'After Tax')
-                      .renderDataPoints({radius: 5})
-                     .colors('red')
-                     .renderTitle(true)
-            ])
-          .legend(dc.legend().x(800).y(10).itemHeight(13).gap(5))*/
-            
             
         //bind the event
         airlineEmissionChart.on('filtered',function(){
@@ -421,6 +433,15 @@ function generateCharts(){
             plotProfitCount();
             dc.redrawAll();
         });
+        
+        airlineDistanceChart.on('filtered',function(){
+            plotAirlineCount();
+            plotDistanceCount();
+            plotEmissionCount();
+            plotProfitCount();
+            dc.redrawAll();
+        });
+        
         airlineBubbleChart.on('filtered',function(){
             plotAirlineCount();
             plotDistanceCount();
@@ -439,28 +460,6 @@ function generateCharts(){
         plotProfitCount();
         dc.renderAll();
          
-         
-         
-         
-        /*airlineDistanceChart.width(990)
-            .height(300)
-            .margins({top: 10, right: 10, bottom: 20, left:35})
-            .dimension(airlineDistanceDimension)
-            .group(airlineDistanceGroup)
-            .transitionDuration(10)
-            .renderTitle(true)
-            .title(function (d) {
-                return  d.key + ": " + d.value;
-            })
-            .centerBar(true)
-            .barPadding(0.05)
-            .outerPadding([1])
-            .x(d3.scale.ordinal().domain(data.map(function (d) {return d.airline; })))
-            .xUnits(dc.units.ordinal)
-            .elasticY(true)
-            .xAxis().tickFormat();*/
-    
-        //airlineDistanceChart.onClick = function() {};
         
     });
     
